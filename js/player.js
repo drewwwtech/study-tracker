db899f1a6b43c66d12ae4772398fa2e146d3ac0b
Jhon Andrew Valerio <xxdeadshot003xx@gmail.com>
2026-06-16 18:08:07 +0800

    Streak updated

--- js/player.js
+++ js/player.js
@@ -29,4 +29,28 @@ export class Player {
         this.xpToNextLevel = (100 * this.level)
     }
 
+    updateStreak(){
+        let today = new Date().toISOString().split('T')[0]
+        let yesterday = new Date()
+        yesterday.setDate(yesterday.getDate() - 1)
+        yesterday = yesterday.toISOString().split('T')[0]
+
+        if (this.lastStudyDate == today) {
+            return null
+        } 
+        else if (this.lastStudyDate == yesterday) {
+            this.currentStreak += 1
+        }
+        else{
+            this.currentStreak = 1
+        }
+
+        this.lastStudyDate = today
+
+        if (this.currentStreak > this.longestStreak) {
+            this.longestStreak = this.currentStreak
+        }
+
+    }
+
 }
