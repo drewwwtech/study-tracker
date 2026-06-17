69d198e162e3704c4ddd4aa9122a5ae174c5b96c
Jhon Andrew Valerio <xxdeadshot003xx@gmail.com>
2026-06-17 21:50:11 +0800

    Connecting player data to the sidebar

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ import { Player } from './player.js'
 
 let hunter = new Player ("Hunter")
 hunter.gainExp(150)
-console.log(hunter)
+
+document.getElementById("player-name").textContent = hunter.name
+document.getElementById("player-level").textContent = `Lvl ${hunter.level}`
