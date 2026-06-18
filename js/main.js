527e6660a576c996168cc6ffe36e57690e85655c
Jhon Andrew Valerio <xxdeadshot003xx@gmail.com>
2026-06-18 19:43:55 +0800

    Pomodoro pt.2 with test on main to see if the time will print

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 import { Player } from './player.js'
+import { Timer } from './timer.js'
 
 let hunter = new Player ("Hunter")
 hunter.gainExp(150)
 
 document.getElementById("player-name").textContent = hunter.name
 document.getElementById("player-level").textContent = `Lvl ${hunter.level}`
+
+let pomodoro = new Timer()
+console.log(pomodoro.getFormattedTime()) // test for timer
--- js/timer.js
+++ js/timer.js
@@ -16,4 +16,43 @@ export class Timer {
     start() {
         this.isRunning = true
     }
-}
+
+    switchMode() {
+        if (this.mode == "study") {
+            this.sessionCompleted += 1
+
+            if (this.sessionCompleted % 4 === 0) {
+                this.mode = "longBreak"
+                this.timeLeft = this.longBreakDuration
+            } else {
+                this.mode = "shortBreak"
+                this.timeLeft = this.shortBreakDuration
+            }
+        } else {
+            this.mode = "study"
+            this.timeLeft = this.studyDuration
+        }
+
+    }
+
+    tick() {
+        if (!this.isRunning) {
+            return
+        }
+        this.timeLeft -= 1
+        if (this.timeLeft == 0) {
+            this.switchMode()
+        }
+        } 
+
+    getFormattedTime() {
+        let minutes = Math.floor(this.timeLeft / 60)
+        let seconds = this.timeLeft % 60
+
+        let minutesStr = minutes.toString().padStart(2, "0")
+        let secondsStr = seconds.toString().padStart(2, "0")
+
+        return `${minutesStr}:${secondsStr}`        
+    }
+
+    }
