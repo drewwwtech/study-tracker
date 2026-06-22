5d93526c6008adfc8ee33e19f5c8aa3189ec7d72
Jhon Andrew Valerio <xxdeadshot003xx@gmail.com>
2026-06-22 21:34:30 +0800

    connecting the start and pause button backend

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 import { Player } from './player.js'
 import { Timer } from './timer.js'
 
-// let hunter = new Player ("Hunter")
-// hunter.gainExp(150)
+let hunter = new Player ("Hunter")
+hunter.gainExp(150)
 
-// document.getElementById("player-name").textContent = hunter.name
-// document.getElementById("player-level").textContent = `Lvl ${hunter.level}`
+document.getElementById("player-name").textContent = hunter.name
+document.getElementById("player-level").textContent = `Lvl ${hunter.level}`
 
 let pomodoro = new Timer()
-pomodoro.start()
-setInterval(() => {
-    pomodoro.tick()
-    console.log(pomodoro.getFormattedTime())
-}, 1000)
+let intervalid = null
+
+document.getElementById("start-btn").addEventListener("click", () => {
+    clearInterval(intervalid)
+    pomodoro.start()
+
+    setInterval(() => {
+        pomodoro.tick()
+        document.getElementById("timer-display").textContent = pomodoro.getFormattedTime()
+    }, 1000)
+})
+
+document.getElementById("pause-btn").addEventListener("click", () => {
+    pomodoro.pause()
+    clearInterval(intervalid)
+})
